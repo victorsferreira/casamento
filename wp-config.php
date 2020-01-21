2380140d8433fc8469e6587f212300e212c45ac4
Victor <victorferreirait@gmail.com>
2020-01-20 21:49:55 -0300

    .htaccess

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+// define('WP_HOME','http://example.com');
+// define('WP_SITEURL','http://example.com');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
